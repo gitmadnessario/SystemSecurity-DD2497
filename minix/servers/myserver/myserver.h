f428001d4fb3b30997ffd1513179747440cec899
madnessario <madness21r21@gmail.com>
2020-01-21 02:25:41 +0100

    build with openssl

--- minix/servers/myserver/myserver.h
+++ minix/servers/myserver/myserver.h
@@ -9,4 +9,15 @@
 #include <minix/param.h>
 #include <regex.h>
 
+#include <lib.h>
+#include <pwd.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/conf.h>
+#include <openssl/cmac.h>
+
+#define BLOCK_SIZE 16
+#define IV_SIZE 16
+#define CMAC_SIZE 16
+
 #endif /* _DS_STORE_H_ */
--- minix/servers/myserver/proto.h
+++ minix/servers/myserver/proto.h
@@ -11,4 +11,20 @@ int do_sys1(message *m_ptr);
 int do_check_code(message *m_ptr);
 int sef_cb_init_fresh(int type, sef_init_info_t *info);
 
+/* function prototypes */
+uid_t getuid(void);
+
+void print_hex(unsigned char *, size_t);
+void print_string(unsigned char *, size_t);
+void keygen(unsigned char *, unsigned char **, unsigned char *);
+int myencrypt(unsigned char *, int, unsigned char *, unsigned char *,
+    unsigned char **);
+int decrypt(unsigned char *, int, unsigned char *, unsigned char *,
+    unsigned char **);
+void gen_cmac(unsigned char *, size_t, unsigned char *, unsigned char **);
+int verify_cmac(unsigned char *, unsigned char *);
+unsigned char* generate_iv(unsigned char*, int);
+void encrypt_entry(unsigned char*, unsigned char*, int);
+void decrypt_entry(unsigned char*, unsigned char*, int);
+
 #endif
